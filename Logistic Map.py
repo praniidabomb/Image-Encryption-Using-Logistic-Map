15b5ef4a794759c381780a2d00d2b5318f5e761f
Pranay Vithlani <pranay2410@gmail.com>
2020-12-02 14:30:46 +0530

    Update Logistic Map.py

--- Logistic Map.py
+++ Logistic Map.py
@@ -4,21 +4,22 @@ import numpy as np
 def decimalToBinary(num):
     return format(num, '08b')
 
-im = Image.open("frame.png").convert('L')
+im = Image.open("Your Image").convert('L')
 image_array = np.array(im)
+row, col = image_array.shape
 
 r = 3.99
-N = 384000
-x = .5 + np.zeros(N)
+N = row * col
+x = 0.5 + np.zeros(N)
 for n in range(N-1):
     x[n+1] = r * x[n] * (1 - x[n])
-y = x.reshape(800, 480)
+y = x.reshape(row, col)
 p = y*255
 z = p.astype('int64')
-imgg2 = np.zeros(384000)
-imgg = imgg2.reshape(800, 480)
-for i in range(800):
-    for j in range(480):
+imgg2 = np.zeros(N)
+imgg = imgg2.reshape(row, col)
+for i in range(row):
+    for j in range(col):
         p = decimalToBinary(image_array[i][j])
         key = decimalToBinary(z[i][j])
         a = [int(x) for x in str(p)]
@@ -28,5 +29,6 @@ for i in range(800):
             string = string + str(a[x] ^ b[x])
         imgg[i][j] = int(string[::-1], 2)
 
-img2 = Image.fromarray(imgg, 'L')
+img2 = Image.fromarray(np.uint8(imgg))
 img2.show()
+img2 = img2.save("Your Cipher Image Name")
